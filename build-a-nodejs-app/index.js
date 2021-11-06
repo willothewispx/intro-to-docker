e404a238c387e772ce77020cf65e6ee966fa38c3
Denes Stolte <contact@stolte.me>
2021-11-06 23:06:31 +0100

    Basic nodejs app

--- build-a-nodejs-app/index.js
+++ build-a-nodejs-app/index.js
@@ -1,8 +1,7 @@
-import http from "http";
+const http = require("http");
 
 http.createServer(function(request, response) {
   console.log("request received");
-  response.write("hello world")
   response.end("hello world", "utf-8")
 }).listen(3000);
 console.log("server started")
